ef0cf240f00f73888f4e0bde773c75e83053bb9e
MikeP <mikep@webpower.com>
2015-11-17 17:46:57 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Usage:
+
+npm test 
+
+npm run deploy
+
+file:///C:/Source/NodeJS/mocha-phantomjs-hosted-jquery/browser/example/example.htm
---
+++ index.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    "use strict";
+    
+    if (typeof (window) !== 'undefined') {
+        var Example = require('./lib/Example.js');    
+        var exampleElements = document.querySelectorAll('[data-example-app]');
+        for (var i=0; i < exampleElements.length; i++) {
+            new Example(exampleElements[i]);
+        }
+    }
+
+})();
